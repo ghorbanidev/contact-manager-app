fe5d44d022dace79d0bfd52abbe8b71efca8a6f6
Ali Ghorbani <119003410+ghorbanidev@users.noreply.github.com>
2023-05-13 21:06:42 +0330

    Update contactService.js

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "https://contact-manager-app-server.vercel.app/";
+const SERVER_URL = "http://localhost:9000";
 
 // @desc Get All Contacts
 // @route GET http://localhost:9000/contacts
